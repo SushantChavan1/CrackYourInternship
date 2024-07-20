c2447a449fb3e5ebf105991688371b155a5eb28a
Sushant Chavan <sushantchavan120@gmail.com>
2024-07-20 21:28:50 +0530

    Day3

---
+++ 75. Sort Colors.cpp
@@ -0,0 +1,48 @@
+// Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+// We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+// You must solve this problem without using the library's sort function.
+
+ 
+
+// Example 1:
+
+// Input: nums = [2,0,2,1,1,0]
+// Output: [0,0,1,1,2,2]
+// Example 2:
+
+// Input: nums = [2,0,1]
+// Output: [0,1,2]
+ 
+
+// Constraints:
+
+// n == nums.length
+// 1 <= n <= 300
+// nums[i] is either 0, 1, or 2.
+ 
+
+// Follow up: Could you come up with a one-pass algorithm using only constant extra space?
+
+
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int z=0,o=0,size=nums.size();
+        for(int num : nums){
+            if(num==0)z++;
+            else if(num==1)o++;
+        }
+        for(int i=0;i<z;i++){
+            nums[i]=0;
+        }
+        for(int i=z;i<z+o;i++){
+            nums[i]=1;
+        }
+        for(int i=z+o;i<size;i++){
+            nums[i]=2;
+        }
+    }
+};
